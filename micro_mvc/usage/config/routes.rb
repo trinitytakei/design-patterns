b461a9d8877264c9c0014a9424bc4ddd77cd96ef
Trinity Takei <trinity.takei@gmail.com>
2019-12-02 21:34:59 +0100

    Fix default route - remove home_controller references completely

--- micro_mvc/usage/config/routes.rb
+++ micro_mvc/usage/config/routes.rb
@@ -1,7 +1,7 @@
 require_relative '../../impl/router.rb'
 
 Routes = Router.new do
-  match '/' => 'home#index'
-  match '/home/index' => 'home#index'
+  match '/' => 'tasks#index'
+  match '/tasks/index' => 'home#index'
   match '/tasks' => 'tasks#index'
 end
