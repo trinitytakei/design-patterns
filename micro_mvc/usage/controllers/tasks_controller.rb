1d7658fe5611e153a5a2e0f8dee62948883abbe2
Trinity Takei <trinity.takei@gmail.com>
2019-12-02 21:35:33 +0100

    Add before/after filter to write header/footer

--- micro_mvc/usage/controllers/tasks_controller.rb
+++ micro_mvc/usage/controllers/tasks_controller.rb
@@ -2,15 +2,14 @@ require_relative '../../impl/controller/application_controller.rb'
 require_relative '../../usage/models/task.rb'
 
 class TasksController < ApplicationController
+  before_action :add_header
+  after_action :add_footer
+
   def index
     response.write <<~HTML
-    <html>
-      </body>
-        <ul>
-          #{task_list(Task.all)}
-        </ul>
-      </body>
-    </html>
+      <ul>
+        #{task_list(Task.all)}
+      </ul>
     HTML
   end
 
@@ -19,5 +18,13 @@ private
     tasks.each_with_object('') { |task, html|
       html << "<li>#{task.name}</li>"
     }.strip
+   end
+
+   def add_header
+     response.write "<h2>This is a header</h2>\n"
+   end
+
+   def add_footer
+     response.write '<h2>This is a footer</h2>'
    end
 end
